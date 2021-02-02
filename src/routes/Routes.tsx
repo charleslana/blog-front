a4dd0ffaf1f7d21a718a2b64a0d323371df550ae
Charles <charleslanop@gmail.com>
2021-02-02 19:55:01 -0300

    Blog front updated v1

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -4,17 +4,20 @@ import Home from "../views/home/Home";
 import About from "../views/about/About";
 import Login from "../views/login/Login";
 import Register from "../views/register/Register";
+import ScrollToTop from "../components/scroll/ScrollToTop";
 
 class Routes extends Component {
 
     render() {
         return (
-            <Switch>
-                <Route path={'/'} exact={true} component={Home}/>
-                <Route path={'/about'} exact={true} component={About}/>
-                <Route path={'/login'} exact={true} component={Login}/>
-                <Route path={'/register'} exact={true} component={Register}/>
-            </Switch>
+            <ScrollToTop>
+                <Switch>
+                    <Route path={'/'} exact={true} component={Home}/>
+                    <Route path={'/about'} exact={true} component={About}/>
+                    <Route path={'/login'} exact={true} component={Login}/>
+                    <Route path={'/register'} exact={true} component={Register}/>
+                </Switch>
+            </ScrollToTop>
         );
     }
 }
--- src/views/about/About.tsx
+++ src/views/about/About.tsx
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
+import './index.css';
 
 class About extends Component {
 
     render() {
         return (
             <div className={'content'}>
-                <h1>About</h1>
+                <div className={'about'}>
+                    <h1>About the Blog</h1>
+                    <p>The blog's idea is to bring content related to technology to cover information and experiences of others in this area.</p>
+                    <p>However, our goal is to bring together research and solutions to problems related to the infinite technologies in the market.</p>
+                    <p>If you want to be part of this union and help everyone who seeks information, you are in the right place.</p>
+                    <p><b>Big hug from collaborator Charles.</b></p>
+                </div>
             </div>
         );
     }
