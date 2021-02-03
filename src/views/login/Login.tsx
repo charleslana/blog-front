500e7102269f503f64b0a5a881bd0c6d38bbf0e5
Charles <charleslanop@gmail.com>
2021-02-03 12:27:49 -0300

    Blog front updated v1

--- src/views/login/Login.tsx
+++ src/views/login/Login.tsx
@@ -8,6 +8,16 @@ class Login extends Component {
             <div className={'content'}>
                 <div className={'login'}>
                     <h1>Login</h1>
+                    <p>Log in to your account</p>
+                    <label>
+                        <input type={'email'} id={'email'} placeholder={'Inform your email'}/>
+                    </label>
+                    <label>
+                        <input type={'password'} id={'password'} placeholder={'Inform your password'}/>
+                    </label>
+                    <label>
+                        <button type={'button'}>Login</button>
+                    </label>
                 </div>
             </div>
         );
--- src/views/register/Register.tsx
+++ src/views/register/Register.tsx
@@ -8,6 +8,22 @@ class Register extends Component {
             <div className={'content'}>
                 <div className={'register'}>
                     <h1>Register</h1>
+                    <p>Register an account to participate in the blog</p>
+                    <label>
+                        <input type={'text'} id={'name'} placeholder={'Inform your name'}/>
+                    </label>
+                    <label>
+                        <input type={'email'} id={'email'} placeholder={'Inform your email'}/>
+                    </label>
+                    <label>
+                        <input type={'password'} id={'password'} placeholder={'Inform your password'}/>
+                    </label>
+                    <label>
+                        <input type={'password'} id={'confirm_password'} placeholder={'Confirm your password'}/>
+                    </label>
+                    <label>
+                        <button type={'button'}>Register</button>
+                    </label>
                 </div>
             </div>
         );
