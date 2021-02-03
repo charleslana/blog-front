79d585989498c4b5ef4b093e23731e08bd21da7e
Charles <charleslanop@gmail.com>
2021-02-03 19:40:53 -0300

    Blog front updated v1

--- src/components/toast/Toast.tsx
+++ src/components/toast/Toast.tsx
@@ -4,6 +4,16 @@ import ToastInterface from "../interfaces/ToastInterface";
 
 class Toast extends Component<ToastInterface> {
 
+    timeout: any;
+
+    componentDidMount() {
+        this.timeout = setTimeout(() => this.closeToast(), 6000);
+    }
+
+    componentWillUnmount() {
+        clearTimeout(this.timeout);
+    }
+
     closeToast() {
         this.props.toastFunction();
     }
